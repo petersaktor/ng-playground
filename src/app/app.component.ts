de0e7e300830502f8f42a52c730eda8ae3b6bfa0
Saktor Peter <peter.saktor@datapac.sk>
2022-04-27 14:52:40 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+// https://stackblitz.com/edit/material-sidenav-example?file=app%2Fsidenav-autosize-example.html
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  isExpanded = true;
+  isShowing = false;
+  showSubmenu: boolean = false;
+  showSubSubMenu: boolean = false;
+
+  mouseenter() {
+    if (!this.isExpanded) {
+      this.isShowing = true;
+    }
+  }
+
+  mouseleave() {
+    if (!this.isExpanded) {
+      this.isShowing = false;
+    }
+  }
+
+}
